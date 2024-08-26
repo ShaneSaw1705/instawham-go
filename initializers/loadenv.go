590964c3a66fd1da7bc6e99a4fe75ade9ccf6865
Shane Curtis <shanecurtis1705@gmail.com>
2024-08-26 16:33:07 +1200

    trying to fix railway

--- initializers/loadenv.go
+++ initializers/loadenv.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -9,8 +10,13 @@ import (
 func LoadEnvVariables() {
 	// Load .env file
 	// godotenv.Load()
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	// // only load the .env file when running locally
+	// check for a RAILWAY_ENVIRONMENT, if not found, code is running locally
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); !exists {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+
 	}
 }
